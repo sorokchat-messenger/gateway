c13783473e2793007c3b9a5a886cf472423e6fe2
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-09 21:35:35 +0300

    feat: authorization controller

--- src/modules/authorization/authorization.controller.ts
+++ src/modules/authorization/authorization.controller.ts
@@ -1,10 +1,25 @@
-import { Body, Controller, Post } from "@nestjs/common";
-import { RegisterDto } from "../../libs/index.js";
+import { Body, Controller, Delete, Get, Post, Put } from "@nestjs/common";
+import { type LoginDto, type RegisterDto } from "../../libs/index.js";
+import { AUTHORIZATION_CONTROLLER } from "@sorokchat-messenger/contracts";
 
-@Controller()
+@Controller(AUTHORIZATION_CONTROLLER.NAME)
 export class AuthorizationController {
-  @Post()
+  @Post(AUTHORIZATION_CONTROLLER.REGISTER)
   public async register(@Body() payload: RegisterDto) {
     return payload;
   }
+
+  @Post(AUTHORIZATION_CONTROLLER.LOGIN)
+  public async login(@Body() payload: LoginDto) {
+    return payload;
+  }
+
+  @Delete(AUTHORIZATION_CONTROLLER.LOGOUT)
+  public async logout() {}
+
+  @Put(AUTHORIZATION_CONTROLLER.REFRESH_TOKENS)
+  public async refreshTokens() {}
+
+  @Get(AUTHORIZATION_CONTROLLER.PROFILE)
+  public async profile() {}
 }
