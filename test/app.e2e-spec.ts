a30c2849a3c7444cf6fbffd3b3d8d962408a6468
Sorokovsky <Sorokovsky0809@gmail.com>
2026-09-09 09:39:13 +0300

    fix: prettier name

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,11 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import request from 'supertest';
-import { AppModule } from './../src/modules/index.js';
+import { Test, TestingModule } from "@nestjs/testing";
+import { INestApplication } from "@nestjs/common";
+import request from "supertest";
+import { AppModule } from "./../src/modules/index.js";
+import { Application } from "express";
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<unknown>;
+describe("AppController (e2e)", () => {
+  let app: INestApplication<Application>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -15,11 +16,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it("/ (GET)", () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get("/")
       .expect(200)
-      .expect('Hello World!');
+      .expect("Hello World!");
   });
 
   afterEach(async () => {
