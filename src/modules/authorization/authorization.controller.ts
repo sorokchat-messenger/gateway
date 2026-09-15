92030f3e775b5d7e59f501596dcd14fb03859a1a
Sorokovsky <Sorokovskys@ukr.net>
2026-09-15 21:52:55 +0300

    feat: setting up cookie

--- src/modules/authorization/authorization.controller.ts
+++ src/modules/authorization/authorization.controller.ts
@@ -6,6 +6,7 @@ import {
   Inject,
   Post,
   Put,
+  Res,
 } from "@nestjs/common";
 import {
   AUTHORIZATION_CONTROLLER,
@@ -15,7 +16,17 @@ import {
   RegisterSchema,
 } from "@sorokchat-messenger/contracts";
 import { AUTHORIZATION_SERVICE_TOKEN } from "../../infrastructure/index.js";
-import { type AuthorizationServiceClient } from "@sorokchat-messenger/microservices";
+import {
+  type LoginResponse,
+  type RefreshTokensResponse,
+  type AuthorizationServiceClient,
+  type RegisterResponse,
+} from "@sorokchat-messenger/microservices";
+import { type Response } from "express";
+import { lastValueFrom } from "rxjs";
+
+type AuthorizationPayload =
+  RegisterResponse | LoginResponse | RefreshTokensResponse;
 
 @Controller(AUTHORIZATION_CONTROLLER.NAME)
 export class AuthorizationController {
@@ -27,13 +38,19 @@ export class AuthorizationController {
   @Post(AUTHORIZATION_CONTROLLER.REGISTER)
   public async register(
     @Body({ schema: RegisterSchema }) payload: RegisterPayload,
+    @Res({ passthrough: true }) response: Response,
   ) {
-    return this.service.register(payload);
+    const result = await lastValueFrom(this.service.register(payload));
+    return this.authorize(result, response);
   }
 
   @Post(AUTHORIZATION_CONTROLLER.LOGIN)
-  public async login(@Body({ schema: LoginSchema }) payload: LoginPayload) {
-    return this.service.login(payload);
+  public async login(
+    @Body({ schema: LoginSchema }) payload: LoginPayload,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const result = await lastValueFrom(this.service.login(payload));
+    return this.authorize(result, response);
   }
 
   @Delete(AUTHORIZATION_CONTROLLER.LOGOUT)
@@ -44,4 +61,18 @@ export class AuthorizationController {
 
   @Get(AUTHORIZATION_CONTROLLER.PROFILE)
   public async profile() {}
+
+  private authorize(
+    { accessToken, refreshToken }: AuthorizationPayload,
+    response: Response,
+  ): { accessToken: string } {
+    response.cookie("__Host-", refreshToken, {
+      domain: undefined,
+      path: "/",
+      secure: true,
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+    return { accessToken };
+  }
 }
